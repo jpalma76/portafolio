f41d9a602f3e9372e1f3c9cf9866884c709f2fdb
Juan Palma <juan.palma.alvarez@gmail.com>
2025-06-10 11:59:25 -0400

    elimina otro portafolio

--- js/index.js
+++ js/index.js
@@ -17,18 +17,16 @@ function visitar() {
     } else if(projects.value == 1) {
         window.open("https://otro-portafolio.netlify.app/", "_blank");
     } else if(projects.value == 2) {
-        window.open("https://portafolio-tres.netlify.app/", "_blank");
-    } else if(projects.value == 3) {
         window.open("https://copia-de-portfolio-dev.netlify.app/", "_blank");
-    } else if(projects.value == 4) {
+    } else if(projects.value == 3) {
         window.open("https://minimalist-portfolio-with-astro.netlify.app/", "_blank");
-    } else if(projects.value == 5) {
+    } else if(projects.value == 4) {
         window.open("https://copia-de-pre-landing-de-la-velada-iv.netlify.app/", "_blank");
-    } else if(projects.value == 6) {
+    } else if(projects.value == 5) {
         window.open("https://renderizado-de-peliculas.netlify.app/", "_blank");
-    } else if(projects.value == 7) {
+    } else if(projects.value == 6) {
         window.open("https://proyecto-react-carrito-de-compras.netlify.app/", "_blank");
-    } else if(projects.value == 8) {
+    } else if(projects.value == 7) {
         window.open("https://github.com/jpalma76", "_blank");
     }
 }
